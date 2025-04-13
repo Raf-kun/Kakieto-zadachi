cdd1aa1144b36304987bc229e599ee09fc86d0f6
Raf-kun <14katosik03@gmail.com>
2025-04-13 18:53:45 +0300

    task сложный :_

--- task3/task3.32.4.py
+++ task3/task3.32.4.py
@@ -1,7 +1,9 @@
-X = int(input("Введите x: "))
-Y = int(input("Введите y: "))
+X = float(input("Введите x: "))
+Y = float(input("Введите y: "))
 
-if (X > 1) and (Y >= 0 or Y <= -1):
-    print("Условия выполняются!)")
+# if (X >= 1 and Y >= 0) or (X > 1 and Y <= -1):
+#     print("Условия выполняются!)")
+if (X >= 1 and X <= 1.9) and (Y <= -1) or (X >= 2 and Y >= 0):
+    print("Условия выполняются!))")
 else:
     print("Условия не выполняются!(")
